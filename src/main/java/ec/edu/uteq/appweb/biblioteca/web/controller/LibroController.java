d2d25915781c26fafe8ec64c67572f8a88e8103c
Johan_Loor <91645452+JohanCarvajal04@users.noreply.github.com>
2026-08-28 09:57:10 -0500

    create

--- src/main/java/ec/edu/uteq/appweb/biblioteca/web/controller/LibroController.java
+++ src/main/java/ec/edu/uteq/appweb/biblioteca/web/controller/LibroController.java
@@ -1,9 +1,20 @@
 package ec.edu.uteq.appweb.biblioteca.web.controller;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ec.edu.uteq.appweb.biblioteca.domain.Autor;
+import ec.edu.uteq.appweb.biblioteca.domain.Libro;
 import ec.edu.uteq.appweb.biblioteca.service.LibroService;
+import ec.edu.uteq.appweb.biblioteca.web.dto.ApiResponse;
+import ec.edu.uteq.appweb.biblioteca.web.dto.AutorRequest;
+import ec.edu.uteq.appweb.biblioteca.web.dto.AutorResponse;
+import ec.edu.uteq.appweb.biblioteca.web.dto.LibroRequest;
+import ec.edu.uteq.appweb.biblioteca.web.dto.LibroResponse;
+import ec.edu.uteq.appweb.biblioteca.web.dto.PageMeta;
 import ec.edu.uteq.appweb.biblioteca.web.mapper.LibroMapper;
 
 /**
@@ -40,7 +51,36 @@ public class LibroController {
     }
     @GetMapping
     public ApiResponse<List<LibroResponse>> Listar (@PageableDefault(size= 20 ) Pageable paginacion){
-        Page<Libro> pagina = servicio.listarActivos(null)
+        Page<Libro> pagina = servicio.listarActivos(paginacion);
+        List<LibroResponse> datos = pagina.getContent().stream().map(mapper::aRespuesta).toList();
+        return ApiResponse.ok(datos, "Libros listados", PageMeta.de(pagina));
+    }
+
+    @GetMapping("/{id}")
+    public ApiResponse<LibroResponse> buscar(@PathVariable Long id) {
+        return ApiResponse.ok(mapper.aRespuesta(servicio.buscarPorId(id)), "Autor encontrado");
+    }
+
+    @GetMapping("/{id}")
+    public ApiResponse<LibroResponse> buscar(@PathVariable String titulo,@PathVariable Long categoriaId,@PathVariable Integer anioDesde) {
+        return ApiResponse.ok(mapper.aRespuesta(servicio.buscar(titulo,categoriaId,anioDesde)), "Autor encontrado");
+    }
+
+
+    @PostMapping
+    @PreAuthorize("hasRole('BIBLIOTECARIO')")
+    public ResponseEntity<ApiResponse<LibroResponse>> crear(@Valid @RequestBody LibroRequest solicitud) {
+        Libro creado = servicio.crear(solicitud.titulo(),solicitud.categoriaId(),solicitud.anioPublicacion());
+        LibroResponse cuerpo = mapper.aRespuesta(creado);
+        return ResponseEntity
+                .created(URI.create("/api/v1/libros/" + creado.getId()))
+                .body(ApiResponse.ok(cuerpo, "Libro creado"));
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ApiResponse<LibroResponse>> desactivar(@Valid @PathVariable Long id){
+
     }
 
 
